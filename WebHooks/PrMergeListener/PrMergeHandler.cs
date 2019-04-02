bda298d6cf8f6c4c1beb84007f98de3dbedc9e42
Michael S <mls@nullptrltd.com>
2019-04-02 18:49:29 -0500

    Working check in

--- WebHooks/PrMergeListener/PrMergeHandler.cs
+++ WebHooks/PrMergeListener/PrMergeHandler.cs
@@ -1,41 +1,67 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Logging;
-using Microsoft.WindowsAzure.Storage;
-using Microsoft.WindowsAzure.Storage.Queue;
-using Newtonsoft.Json;
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="PrMergeHandler.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   TODO The pr merge handler.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace PrMergeListener
-{
-    public static class PrMergeHandler
     {
-        [FunctionName("PrMergeHandler")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
-            [Queue("pr-merge-queue")]CloudQueue outputQueue,
-            ILogger log)
-        {
-            log.LogInformation("A WebHook Event has been caught");
-            
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+        #region
 
-            //Add message to queue and make it visible after a specific time.
-            await outputQueue.AddMessageAsync(new CloudQueueMessage(requestBody),
-                timeToLive: TimeSpan.FromDays(2),
-                initialVisibilityDelay: null,
-                options: null,
-                operationContext: null);
+        using System;
+        using System.IO;
+        using System.Threading.Tasks;
 
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            
-            return requestBody != null
-                ? (ActionResult)new OkObjectResult("WebHook Event Processed")
-                : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
-        }
+        using Microsoft.AspNetCore.Http;
+        using Microsoft.AspNetCore.Mvc;
+        using Microsoft.Azure.WebJobs;
+        using Microsoft.Azure.WebJobs.Extensions.Http;
+        using Microsoft.Extensions.Logging;
+        using Microsoft.WindowsAzure.Storage.Queue;
+
+        using Newtonsoft.Json;
+
+        #endregion
+
+        /// <summary>
+        /// TODO The pr merge handler.
+        /// </summary>
+        public static class PrMergeHandler
+            {
+                /// <summary>
+                /// TODO The run.
+                /// </summary>
+                /// <param name="req">
+                /// TODO The req.
+                /// </param>
+                /// <param name="outputQueue">
+                /// TODO The output queue.
+                /// </param>
+                /// <param name="log">
+                /// TODO The log.
+                /// </param>
+                /// <returns>
+                /// The <see cref="Task"/>.
+                /// </returns>
+                [FunctionName("PrMergeHandler")] public static async Task<IActionResult> Run(
+                    [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)]
+                    HttpRequest req,
+                    [Queue("pr-merge-queue")] CloudQueue outputQueue,
+                    ILogger log)
+                    {
+                        log.LogInformation("A WebHook Event has been caught");
+
+                        var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+                        // Add message to queue and make it visible after a specific time.
+                        await outputQueue.AddMessageAsync(new CloudQueueMessage(requestBody), TimeSpan.FromDays(2), null, null, null);
+
+                        dynamic data = JsonConvert.DeserializeObject(requestBody);
+
+                        return requestBody != null ? (ActionResult)new OkObjectResult("WebHook Event Processed") : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
+                    }
+            }
     }
-}
